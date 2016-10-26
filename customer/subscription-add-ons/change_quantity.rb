6bd4f3ddf2bcb73bdccbab8ecc44455f0b734dc2
anurag-infiverve <anurag.dubey@infiverve.com>
2016-10-26 15:59:51 +0530

    subscription-add-ons folder added and flintbits modified

---
+++ customer/subscription-add-ons/change_quantity.rb
@@ -0,0 +1,44 @@
+require 'json'
+@log.trace("Started execution 'flint-o365:customer:subscription:change_subscription_quantity.rb' flintbit...") 
+begin
+
+     # Mandatory
+     @connector_name = @input.get('connector-name') 			# Name of the office365 Connector
+     @action = 'change-subscription-add-on-quantity'				#@input.get('action')			                
+     @microsoft_id = @input.get('customer-id') 			        # id of the Microsoft Account
+     @subscriptionId=@input.get('subscription-id')
+     @quantity=@input.get('quantity')
+     @microsoftCloudActionUrl = '/MSCustomerSubscriptionAddOn/performOperations'  
+
+     @log.info("Flintbit input parameters are, connector name :: #{@connector_name} | Customer ID::#{@microsoft_id} | Subscription Id :: #{@subscriptionId} | Quantity :: #{@quantity}")
+
+     response = @call.connector(@connector_name)
+                     .set('action', 'change-subscription-quantity')
+                     .set('microsoft-id', @microsoft_id)
+                     .set('subscription-id',@subscriptionId)
+                     .set('quantity',@quantity.to_i)
+                     .sync
+
+     response_exitcode = response.exitcode # Exit status code
+     response_message =  response.message # Execution status message
+
+     response_body = JSON.parse(response.get('body')) # parsing json string to JSON Object
+     response_body['action'] = @action 
+     response_body['customer-id'] = @microsoft_id
+     response_body['subscription-add-on-id'] = response_body['id']   
+
+     @log.info("#{response_body}")
+
+       if response_exitcode==0
+      	     @log.info("Success in executing #{@connector_name} Connector, where exitcode :: #{response_exitcode} | message :: #{response_message}")
+             @output.set("result::",response_body)
+             @call.bit('flint-o365:http:http_request.rb').set('method', 'POST').set('url', @microsoftCloudActionUrl).timeout(120000).set('body', response_body.to_json).sync
+       else
+         @log.error("ERROR in executing #{@connector_name} where, exitcode :: #{response_exitcode} | message :: #{response_message}")
+         @output.exit(1, response_message)
+       end
+rescue Exception => e
+     @log.error(e.message)
+     @output.set('exit-code', 1).set('message', e.message)
+end
+@log.trace("Finished execution 'flint-o365:customer:subscription:change_subscription_quantity.rb' flintbit...") 
---
+++ customer/subscription-add-ons/suspend.rb
@@ -0,0 +1,47 @@
+require 'json'
+@log.trace("Started execution'flint-o365:customer:subscription:suspend_subscription.rb' flintbit...")
+begin
+
+     @connector_name = @input.get('connector-name') 		# Name of Connector
+     @action = 'suspend-subscription-add-on'				#@input.get('action')            		        
+     @microsoft_id = @input.get('customer-id')		        # id of the Microsoft Account
+     @subscription_id=@input.get('subscription-id')
+     @microsoftCloudActionUrl = '/MSCustomerSubscriptionAddOn/performOperations'  
+
+     @log.info("Flintbit input parameters are, connector name :: #{@connector_name} | Customer ID::#{@id} | Subscription Id :: #{@subscription_id} ")
+
+     response = @call.connector(@connector_name)
+                     .set('action', 'suspend-subscription')
+                     .set('microsoft-id', @microsoft_id)
+                     .set('subscription-id', @subscription_id)
+                     .sync
+
+     response_exitcode = response.exitcode # Exit status code
+     response_message =  response.message # Execution status message
+     response_body     =  response.get('body')
+     #response_body = JSON.parse(response.get('body')) # parsing json string to JSON Object
+
+     @log.info(response_body)
+
+     if response_exitcode==0
+         response_body = JSON.parse(response.get('body')) # parsing json string to JSON Object
+         response_body['action'] = @action
+         response_body['customer-id'] = @microsoft_id
+         response_body['subscription-add-on-id'] = response_body['id']       
+
+         @log.info("Success in executing #{@connector_name} Connector, where exitcode :: #{response_exitcode} | message :: #{response_message}")
+         @output.set("result::",response_body)
+         @call.bit('flint-o365:http:http_request.rb').set('method', 'POST').set('url', @microsoftCloudActionUrl).timeout(120000).set('body', response_body.to_json).sync
+
+     else
+         @log.error("ERROR in executing #{@connector_name} where, exitcode :: #{response_exitcode} | message :: #{response_message}")
+         @output.exit(1, response_message)
+     end
+
+ rescue Exception => e
+     @log.error(e.message)
+     @output.set('exit-code', 1).set('message', e.message)
+
+ end
+
+@log.trace("Finished execution 'flint-o365:customer:subscription:suspend_subscription.rb' flintbit...") 
